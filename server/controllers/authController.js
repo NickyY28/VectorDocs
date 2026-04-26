c06d95feed38387afb3707abd2c2456b58de275f
NickyY28 <nicky28bt@gmail.com>
2026-04-26 20:54:46 +0530

    Fix register function response and implement login function structure

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,10 +14,22 @@ export const register = async (req,res) => {
             return res.status(400).json({message: "email already registered"});
         const user = await User.create({name, email, password });
         const token = generateToken(user._id);
+        res.status(201).json({
+        token,
+        user: { id: user._id, name: user.name, email: user.email },
+        })
     } catch (error) {
-        res.status(500).json({message:"Server error", error: err.message});
+        res.status(500).json({message:"Server error", error: error.message});
     }
 };
 
 // POST /api/auth/login
+export const login = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+    } catch (error) {
+        res.status(500).json({message: "server error", error: error.message})
+    }
+}
+
 export const 
