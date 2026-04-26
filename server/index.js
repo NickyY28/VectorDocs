6ef0aa5dc680eb269983def99983576a0b8d8208
NickyY28 <nicky28bt@gmail.com>
2026-04-26 20:08:38 +0530

    Fix route paths and improve authMiddleware token validation

--- server/index.js
+++ server/index.js
@@ -14,14 +14,14 @@ const app = express();
 //middleware
 app.use(cors({origin:"http://localhost:5173", credentials: true}));
 app.use(express.json());
-
+//app.use works for all the requestes -> get, post,put, delete
 //routes
-app.use("api/auth", authRoutes);
-app.use("api/pdf", pdfRoutes);
-app.use("api/chat", chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/pdf", pdfRoutes);
+app.use("/api/chat", chatRoutes);
 
 // Health check
-app.get("api/health", (req,res)=> res.json({status: "ok"}));
+app.get("/api/health", (req,res)=> res.json({status: "ok"}));
 
 //start server
 const startServer = async () =>{
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 const authMiddleware = (req,res,next) =>{
     const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith("Bearer")){
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
         return res.status(401).json({message: "No token provided"});
     }
     const token = authHeader.split(" ")[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.id;
+        req.userId = decoded.id;  //attaching user to request so that every route after this knows which user is making the request
         next();
     } catch (error) {
         return res.status(401).json({message : "Invalid or expired token"});
