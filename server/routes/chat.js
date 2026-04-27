02027bf15d77ea3a7aa06af57a57a9ed97085fd2
NickyY28 <nicky28bt@gmail.com>
2026-04-27 07:42:31 +0530

    chat route

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,9 +1,11 @@
 import express from "express";
- 
+import authMiddleware from "../middleware/authMiddleware.js";
+import { sendMessage, getChatHistory, clearChat } from "../controllers/chatController.js";
+
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.json({message: "chat route working"});
-});
+router.get("/:pdfId", authMiddleware, getChatHistory);
+router.post("/:pdfId", authMiddleware, sendMessage);
+router.delete("/:pdfId", authMiddleware, clearChat);
 
 export default router;
